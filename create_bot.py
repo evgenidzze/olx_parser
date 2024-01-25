7b1ecafe810f22ebab93d7c672813098ec479582
evgenidzze <evgeniysmetanyuk@gmail.com>
2024-01-25 16:21:17 +0200

    init

--- create_bot.py
+++ create_bot.py
@@ -8,7 +8,7 @@ from apscheduler_di import ContextSchedulerDecorator
 job_stores = {
     "default": RedisJobStore(
         jobs_key="dispatched_trips_jobs", run_times_key="dispatched_trips_running",
-        host="localhost", port=6379
+        host="localhost", port=6380
     )
 }
 scheduler = ContextSchedulerDecorator(AsyncIOScheduler(jobstores=job_stores))
